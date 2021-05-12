878052213334760e46211c87848968fe95844e1e
MedHedii <79151270+MedHedii@users.noreply.github.com>
2021-05-12 02:23:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`The Server is running on port ${port}`);
+});
+
+function logger(req, res, next) {
+    console.table({ method: req.method, path: req.url });
+    var date = new Date();
+    if (
+        date.getHours() < 9 ||
+        date.getHours() > 17 ||
+        date.getDate() == 0 ||
+        date.getDate() == 5
+    ) {
+        next(res.send("we are closed"));
+    } else {
+        next();
+    }
+}
+
+app.use(logger);
+
+app.use(express.static(__dirname + "/Pub"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/Pub/home.html");
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/Pub/contact.html");
+});
+
+app.get("/service", (req, res) => {
+    res.sendFile(__dirname + "/Pub/service.html");
+});
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(__dirname + "/Pub/stlye.css");
+});
